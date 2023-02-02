ff89a8842791fbdd25ee82a2ecd51374a83d450f
Quinn Collins <collinsqui@gmail.com>
2023-02-02 12:36:53 -0500

    Update view handler to use snippet struct for templates and render a snippet inside a new view template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,9 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+	"text/template"
 
 	"snippetbox.qcollins.net/internal/models"
 )
@@ -16,21 +16,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  files := []string{
-    "./ui/html/base.tmpl.html",
-    "./ui/html/partials/nav.tmpl.html",
-    "./ui/html/pages/home.tmpl.html",
-  }
-
-  ts, err := template.ParseFiles(files...)
+  snippets, err := app.snippets.Latest()
   if err != nil {
     app.serverError(w, err)
     return
   }
-  err = ts.ExecuteTemplate(w, "base", nil)
-  if err != nil {
-    app.serverError(w, err)
+
+  for _, snippet := range snippets {
+    fmt.Fprintf(w, "%+v\n", snippet)
   }
+
+  // files := []string{
+  //   "./ui/html/base.tmpl.html",
+  //   "./ui/html/partials/nav.tmpl.html",
+  //   "./ui/html/pages/home.tmpl.html",
+  // }
+
+  // ts, err := template.ParseFiles(files...)
+  // if err != nil {
+  //   app.serverError(w, err)
+  //   return
+  // }
+  // err = ts.ExecuteTemplate(w, "base", nil)
+  // if err != nil {
+  //   app.serverError(w, err)
+  // }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +60,26 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  fmt.Fprintf(w, "%+v", snippet)
+  files := []string{
+    "./ui/html/base.tmpl.html",
+    "./ui/html/partials/nav.tmpl.html",
+    "./ui/html/pages/view.tmpl.html",
+  }
+
+  ts, err := template.ParseFiles(files...)
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+
+  data := &templateData{
+    Snippet: snippet,
+  }
+
+  err = ts.ExecuteTemplate(w, "base", data)
+  if err != nil {
+    app.serverError(w, err)
+  }
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "snippetbox.qcollins.net/internal/models"
+
+type templateData struct {
+  Snippet *models.Snippet
+}
